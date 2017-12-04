2d3403564f6ecb8829fa13b6d1c60a0f8a479443
Ben Steinberg <ben@potswift.org>
2017-12-03 22:49:59 -0500

    Test lapsed link

--- test/FlahspaperWarpSpec.hs
+++ test/FlahspaperWarpSpec.hs
@@ -3,7 +3,7 @@ module FlahspaperWarpSpec (spec) where
 import Flahspaper
 import Test.Hspec
 import qualified Data.Map.Strict as Map
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM
 import Network.Wai.Handler.Warp (run)
 import Network.Wreq
@@ -19,7 +19,7 @@ runApp :: IO ()
 runApp = do
   let sm = Map.empty :: SecretMap
   secrets <- newTVarIO sm
-  let sopts = Flahspaper.Options 2 2048
+  let sopts = Flahspaper.Options 1 2048
   _ <- ($) forkIO $ janitor secrets sopts
   _ <- ($) forkIO $ run 8080 $ app secrets sopts
   putStrLn "  Started server..."
@@ -44,7 +44,9 @@ spec = beforeAll runApp $ do
       (r ^. responseStatus . statusCode) `shouldBe` 200
     it "GET /badness" $ do
       r'' <- getWith opts "http://localhost:8080/badness"
-      (r'' ^. responseBody) `shouldBe` BLC.pack "You are likely to be eaten by a grue"
+      (r'' ^. responseBody) `shouldBe`
+        BLC.pack "You are likely to be eaten by a grue"
+
   describe "Round-trip testing" $ do
     it "Create and retrieve a secret" $ do
       -- create a secret
@@ -60,6 +62,7 @@ spec = beforeAll runApp $ do
       (r'' ^. responseStatus . statusCode) `shouldBe` 404
       (r'' ^. responseBody) `shouldBe`
         BLC.pack "You are likely to be eaten by a grue"
+
     it "Create and retrieve a secret file" $ do
       -- upload a file
       r <- post' "/addfile" (partFile (T.pack "file") "LICENSE")
@@ -73,12 +76,28 @@ spec = beforeAll runApp $ do
       (r'' ^. responseStatus . statusCode) `shouldBe` 404
       (r'' ^. responseBody) `shouldBe`
         BLC.pack "You are likely to be eaten by a grue"
+  describe "Test error conditions" $ do
     it "Try to upload too large a file" $ do
       r <- post' "/addfile" (partFile (T.pack "file") "README.md")
       (r ^. responseStatus . statusCode) `shouldBe` 200
       let msg = getH2 $ r ^. responseBody
       msg `shouldBe` BLC.pack "Upload too large."
+
+    it "Make a secret and let it lapse" $ do
+      -- create a secret
+      r <- post' "/add" [BC.pack "secret" := "Hello"]
+      let url = getH2 $ r ^. responseBody
+      (r ^. responseStatus . statusCode) `shouldBe` 200
+      -- wait two seconds
+      threadDelay 2000000
+      -- try to retrieve it
+      r' <- getWith opts $ BLC.unpack url
+      (r' ^. responseStatus . statusCode) `shouldBe` 404
+      (r' ^. responseBody) `shouldBe`
+        BLC.pack "You are likely to be eaten by a grue"
+
       where getH2 =
               innerText . take 2 . dropWhile (~/= "<h2>") . parseTags
             -- https://stackoverflow.com/a/34290005 plus tweak
-            opts = set Network.Wreq.checkResponse (Just $ \_ _ -> return ()) defaults
+            opts = set Network.Wreq.checkResponse
+              (Just $ \_ _ -> return ()) defaults
